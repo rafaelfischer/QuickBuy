3699212f5e805529893402eb0a8ddd91d5683170
AndarilhoL27 <leorochasilva27@gmail.com>
2020-02-06 15:30:35 -0300

    Adicionando pacote NuGet Entity pro MYSQL e Proxie para uso de Lazy Load, Criando os Context

---
+++ QuickBuy.Repositorio/Contexto/QuickBuyContexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickBuy.Dominio.Entidades;
+using QuickBuy.Dominio.ObjedoDeValor;
+
+namespace QuickBuy.Repositorio.Contexto
+{
+    public class QuickBuyContexto : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<ItemPedido> ItensPedidos { get; set; }
+        public DbSet<FormaPagamento> FormasPagamentos { get; set; }
+    }
+}
