0d6012ba6d66006679761447dc0b2921fb070bed
Rafael de Brito Fischer <rafaelfischer@gmail.com>
2020-05-31 00:36:58 -0300

    Criacao do componente de login

--- QuickBuy.Web/ClientApp/src/app/app.module.ts
+++ QuickBuy.Web/ClientApp/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { ProdutoComponent } from './produto/produto.component';
 
@@ -18,7 +19,8 @@ import { ProdutoComponent } from './produto/produto.component';
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-    ProdutoComponent
+    ProdutoComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -29,6 +31,7 @@ import { ProdutoComponent } from './produto/produto.component';
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
       { path: 'produto', component: ProdutoComponent },
+      { path: 'login', component: LoginComponent },
     ])
   ],
   providers: [],
